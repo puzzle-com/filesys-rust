979d8f208391893b517c524d9c318fe9e773fc7e
luomijie <zhiwei-luo@hotmail.com>
2019-05-04 16:26:23 +0800

    add test code

--- chain/src/main.rs
+++ chain/src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
-    unsafe { say_hello(); }
     alphanet::start();
 }
--- test-utils/testcgo/src/main.rs
+++ test-utils/testcgo/src/main.rs
@@ -1,10 +1,3 @@
-#[link(name = "test", kind = "static")]
-extern "C" {
-    pub fn say_hello();
-}
-
 fn main() {
-    unsafe {
-        say_hello();
-    }
+
 }
