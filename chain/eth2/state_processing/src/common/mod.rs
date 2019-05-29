d5e83c46a1ece3aabdcc48042caf3078af3d9fea
rooat <welling1234@gmail.com>
2019-05-29 10:47:10 +0800

    update beacon

--- chain/eth2/state_processing/src/common/mod.rs
+++
@@ -1,7 +0,0 @@
-mod exit_validator;
-mod slash_validator;
-mod verify_bitfield;
-
-pub use exit_validator::exit_validator;
-pub use slash_validator::slash_validator;
-pub use verify_bitfield::verify_bitfield_length;
